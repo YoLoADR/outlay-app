61e1bbddd5ed197b254f9106c929d7b435dbe1b0
13091987Y <atticot.yohann@gmail.com>
2018-01-02 10:49:56 +0100

    initiate Outlay Project

---
+++ README.md
@@ -0,0 +1,4 @@
+# muddle-app
+
+# babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+# live-server public
---
+++ src/app.js
@@ -0,0 +1,7 @@
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'
+import './styles/style.scss'
+
+ReactDOM.render(<p>Tu vas en ressortir grandit ! </p>, document.getElementById('app'));
